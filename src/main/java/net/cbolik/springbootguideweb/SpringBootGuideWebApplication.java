e57597fba6b564195656988c2bac2b91ee719807
cbolik <cbolik@gmx.net>
2022-11-28 07:24:51 +0100

    Initial commit

---
+++ src/main/java/net/cbolik/springbootguideweb/SpringBootGuideWebApplication.java
@@ -0,0 +1,13 @@
+package net.cbolik.springbootguideweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGuideWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGuideWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cbolik/springbootguideweb/SpringBootGuideWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cbolik.springbootguideweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGuideWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
